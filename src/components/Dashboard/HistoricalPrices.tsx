ed0e59b9e4a7a24098c35da2f33e8c86964d58a6
jasmin <chjasmin27@gmail.com>
2025-05-16 21:35:03 +0600

    reslove marge conflict

--- src/components/Dashboard/HistoricalPrices.tsx
+++ src/components/Dashboard/HistoricalPrices.tsx
@@ -3,11 +3,7 @@ import React from "react";
 const HistoricalPrices = () => {
   return (
     <div>
-<<<<<<< HEAD
       <div className="2xl:p-10 p-5 rounded border-t-5 border-purple-500 shadow-sm 2xl:h-[25rem] 2xl:w-[22rem] h-[18rem] flex flex-col">
-=======
-      <div className="2xl:p-10 p-5 rounded border-t-5 border-purple-500 shadow-sm 2xl:h-[30rem] 2xl:w-[22rem] h-[18rem] flex flex-col">
->>>>>>> 64c16e2b3078c15820fa676bd2190f667209c9dd
         {/* Fixed Heading */}
         <div className="2xl:pb-4">
           <p className="text-lg">Historical Daily Prices</p>
