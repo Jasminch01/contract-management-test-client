322f23b960d7bab29b79cb282bff13fcddf9fedc
jasmin <chjasmin27@gmail.com>
2025-05-18 22:19:14 +0600

    delete comfirmation modal done

--- src/app/contract-management/page.tsx
+++ src/app/contract-management/page.tsx
@@ -492,7 +492,7 @@ const ContractManagementPage = () => {
 
       {/* Delete Confirmation Modal */}
       {isDeleteConfirmOpen && (
-        <div className="fixed inset-0 bg-black bg-opacity-20 flex items-center justify-center z-50">
+        <div className="fixed inset-0 bg-opacity-20 flex items-center justify-center z-50">
           <div className="bg-white rounded-lg shadow-lg max-w-md w-full">
             <div className="px-5 py-3 border-b border-[#D3D3D3]">
               <h3 className="text-lg font-semibold flex gap-x-5 items-center">
